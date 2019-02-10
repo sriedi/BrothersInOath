22451fbab3ac7d7909e76060b33f4566d2ed8fd0
Sebastian Riedi <sebi.riedi@gmail.com>
2019-02-10 20:03:25 +0100

    Initial commit

---
+++ common/defines/bio_defines.lua
@@ -0,0 +1,18 @@
+NDefines.NDiplomacy.MAX_DIPLO_DISTANCE = 2000 
+--Technology--
+NDefines.NTechnology.DONT_EXECUTE_TECH_BEFORE = 1260	
+NDefines.NTechnology.IDEAL_YEAR_LEVEL_0 = 1260
+NDefines.NTechnology.IDEAL_YEAR_LEVEL_8 = 1500	
+
+
+--Camera Entry Animation
+NDefines.NFrontend.CAMERA_LOOKAT_X = 1739
+NDefines.NFrontend.CAMERA_START_X = 1739
+NDefines.NFrontend.CAMERA_END_X = 1739
+--Frontend Camera Position 
+NDefines.NFrontend.FRONTEND_POS_X = 1739
+NDefines.NFrontend.FRONTEND_LOOK_X = 1739
+NDefines.NFrontend.SETTINGS_POS_X = 1780
+NDefines.NFrontend.SETTINGS_LOOK_X = 1780
+NDefines.NFrontend.MP_OPTIONS_POS_X = 1739
+NDefines.NFrontend.MP_OPTIONS_LOOK_X = 1739
